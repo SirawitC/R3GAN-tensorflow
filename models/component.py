0eae04cf7f7b6511a4b597bc9a549d68d2edcb16
SirawitC <sirawitchok@hotmail.com>
2025-07-25 21:02:53 +0700

    feature: finish ResidualBlock add UpsamplerLayer and DownsamplerLayer

--- models/component.py
+++ models/component.py
@@ -29,18 +29,8 @@ class BiasedActivation(tf.keras.layers.Layer):
 
         # Activation setup
         self.activation = activation.lower()
-        if self.activation == 'lrelu':
-            self.act_fn = lambda x: tf.nn.leaky_relu(x, alpha=0.2)
-            default_gain = math.sqrt(2.0 / (1 + 0.2 ** 2))
-        elif self.activation == 'relu':
-            self.act_fn = tf.nn.relu
-            default_gain = math.sqrt(2.0)
-        elif self.activation == 'linear' or self.activation is None:
-            self.act_fn = lambda x: x
-            default_gain = 1.0
-        else:
-            raise ValueError(f"Unsupported activation: {activation}")
-
+        self.act_fn = self._get_activation_function(self.activation)
+        default_gain = self.get_default_gain(self.activation)
         self.gain = gain if gain is not None else default_gain
 
     def call(self, x):
@@ -48,9 +38,32 @@ class BiasedActivation(tf.keras.layers.Layer):
         x = tf.nn.bias_add(x, self.bias)
         # Apply activation and gain
         return self.act_fn(x) * self.gain
+    
+    @staticmethod
+    def get_default_gain(activation):
+        activation = activation.lower()
+        if activation == 'lrelu':
+            return math.sqrt(2.0 / (1 + 0.2 ** 2))
+        elif activation == 'relu':
+            return math.sqrt(2.0)
+        elif activation == 'linear' or activation is None:
+            return 1.0
+        else:
+            raise ValueError(f"Unsupported activation: {activation}")
+
+    @staticmethod
+    def _get_activation_function(activation):
+        if activation == 'lrelu':
+            return lambda x: tf.nn.leaky_relu(x, alpha=0.2)
+        elif activation == 'relu':
+            return tf.nn.relu
+        elif activation == 'linear' or activation is None:
+            return lambda x: x
+        else:
+            raise ValueError(f"Unsupported activation: {activation}")
 
 
-class Convolution(tf.keras.layers.Layer):
+class Convolution(keras.layers.Layer):
     def __init__(self, input_channels, output_channels, kernel_size, groups=1, activation_gain=1.0):
         super().__init__()
         self.groups = groups
@@ -71,9 +84,7 @@ class Convolution(tf.keras.layers.Layer):
 
     def call(self, x):
         return self.conv(x)
-
-###### TODO: finish this class #######
-class ResidualBlock(keras.Model):
+class ResidualBlock(keras.layers.Layer):
     def __init__(
         self,
         input_channels,
@@ -85,9 +96,18 @@ class ResidualBlock(keras.Model):
         super().__init__()
         num_linear_layers = 3
         expanded_channels = input_channels * expansion_factor
-        activation_gain = biased_activation_gain * variance_scaling ** (-1 / (2 * num_linear_layers - 2))
+        activation_gain = BiasedActivation.get_default_gain('lrelu') * variance_scaling ** (-1 / (2 * num_linear_layers - 2))
 
         self.conv1 = Convolution(input_channels, expanded_channels, kernel_size=1, activation_gain=activation_gain)
         self.conv2 = Convolution(expanded_channels, expanded_channels, kernel_size=kernel_size, groups=cardinality, activation_gain=activation_gain)        
         self.conv3 = Convolution(expanded_channels, input_channels, kernel_size=1, activation_gain=0)
 
+        self.biasact1 = BiasedActivation(expanded_channels)
+        self.biasact2 = BiasedActivation(expanded_channels)
+
+    def call(self, x):
+        y = self.conv1(x)
+        y = self.conv2(self.biasact1(y))
+        y = self.conv3(self.biasact2(y))
+        
+        return x + y
